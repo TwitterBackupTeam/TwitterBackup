f8abb79ad4833c7d3d613482b236b342a22186f4
Кристиян Жеков <k.zhekow@gmail.com>
2018-04-16 14:57:58 +0300

    added initial project

---
+++ TwitterBackup/TwitterBackup.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TwitterBackup.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
