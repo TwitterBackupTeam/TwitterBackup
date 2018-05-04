c8659191948bb12a948fb6716bedc47771fb3eff
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-04 22:45:22 +0300

    Outlining view models for statistics

---
+++ TwitterBackup/TwitterBackup.Web/Areas/Admin/Models/Statistics/DeletedFavouriteTweetersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitterBackup.Web.Areas.Admin.Models.Statistics
+{
+    public class DeletedFavouriteTweetersViewModel
+    {
+    }
+}
