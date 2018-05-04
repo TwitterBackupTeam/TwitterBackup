ca13a0dd8683d249152789c3c212343c86404cf4
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-04 21:35:10 +0300

    Fleshing out interface for favourite tweeters statistics

--- TwitterBackup/TwitterBackup.Data.Services/ServiceInterfaces/StatisticsServices/IFavouriteTweetersStatisticsService.cs
+++ TwitterBackup/TwitterBackup.Data.Services/ServiceInterfaces/StatisticsServices/IFavouriteTweetersStatisticsService.cs
@@ -1,7 +1,12 @@
-﻿namespace TwitterBackup.Data.Services.ServiceInterfaces.StatisticsServices
+﻿using System.Collections.Generic;
+using TwitterBackup.Data.DTO.StatisticsDTOs;
+
+namespace TwitterBackup.Data.Services.ServiceInterfaces.StatisticsServices
 {
 	public interface IFavouriteTweetersStatisticsService
     {
+		IEnumerable<FavouriteTweeterDTO> GetFavouriteTweetersByUserId(long userId);
 
+		IEnumerable<DeletedFavouriteTweeterDTO> GetDeletedFavouriteTweetersByUserId(long userId);
 	}
 }
