5bb5303f20c1f28bd75547bd18d3aef1dfcd04e5
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-04 23:02:02 +0300

    Completing IActionResult methods in statistics controller

--- TwitterBackup/TwitterBackup.Web/Areas/Admin/Controllers/StatisticsController.cs
+++ TwitterBackup/TwitterBackup.Web/Areas/Admin/Controllers/StatisticsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TwitterBackup.Data.Services.ServiceInterfaces.StatisticsServices;
+using TwitterBackup.Web.Areas.Admin.Models.Statistics;
 
 namespace TwitterBackup.Web.Areas.Admin.Controllers
 {
@@ -28,34 +29,52 @@ namespace TwitterBackup.Web.Areas.Admin.Controllers
 		{
 			var favouriteTweeters = this.tweetersStatisticsService.GetFavouriteTweetersByUserId(userId);
 
-			//////
-			return this.View();//////////
+			var viewModel = new FavouriteTweetersViewModel
+			{
+				FavouriteTweetersModels = favouriteTweeters,
+				UserId = userId
+			};
+
+			return this.View(viewModel);
 		}
 
 		public IActionResult DeletedFavouriteTweeters(long userId)
 		{
 			var deletedFavouriteTweeters = this.tweetersStatisticsService.GetDeletedFavouriteTweetersByUserId(userId);
 
+			var viewModel = new DeletedFavouriteTweetersViewModel
+			{
+				DeletedFavouriteTweetersModels = deletedFavouriteTweeters,
+				UserId = userId
+			};
 
-			//////////
-
-			return this.View();//////
+			return this.View(viewModel);
 		}
 
 		public IActionResult StoredTweets(long userId)
 		{
 			var storedTweets = this.tweetsStatisticsService.GetStoredTweetsByUserId(userId);
 
-			//////////
-			return this.View();/////////
+			var viewModel = new StoredTweetsViewModel
+			{
+				StoredTweetsModels = storedTweets,
+				UserId = userId
+			};
+
+			return this.View(viewModel);/////////
 		}
 
 		public IActionResult DeletedTweets(long userId)
 		{
 			var deletedTweets = this.tweetsStatisticsService.GetDeletedTweetsByUserId(userId);
 
-			///////
-			return this.View();////////
+			var viewModel = new DeletedTweetsViewModel
+			{
+				DeletedTweetsModels = deletedTweets,
+				UserId = userId
+			};
+
+			return this.View(viewModel);////////
 		}
 	}
 }
