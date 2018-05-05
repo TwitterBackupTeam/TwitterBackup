17bfb7d8392dffd7b9065a7fb74d741521181a94
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-05 08:30:45 +0300

    Outlined view models for user statistics

---
+++ TwitterBackup/TwitterBackup.Web/Areas/Admin/Models/Users/ListUsersAndRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwitterBackup.Web.Areas.Admin.Models.Users
+{
+    public class ListUsersAndRolesViewModel
+    {
+    }
+}
---
+++ TwitterBackup/TwitterBackup.Web/Areas/Admin/Models/Users/UpdateUserRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TwitterBackup.Web.Areas.Admin.Models.Users
+{
+	public class UpdateUserRoleViewModel
+    {
+		public class AddUserToRoleViewModel
+		{
+			[Required]
+			public long UserId { get; set; }
+
+			[Required]
+			public string Role { get; set; }
+		}
+	}
+}
