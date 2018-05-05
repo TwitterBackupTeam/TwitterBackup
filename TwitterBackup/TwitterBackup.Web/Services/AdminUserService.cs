3bea60e16814e98b590a06c6c31470ca7c2b1617
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-05 19:51:30 +0300

    Implementing IAdminUserService methods

--- TwitterBackup/TwitterBackup.Web/Services/AdminUserService.cs
+++ TwitterBackup/TwitterBackup.Web/Services/AdminUserService.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TwitterBackup.Data.DTO;
 using TwitterBackup.Data.Models;
 using TwitterBackup.Data.Repository;
 using TwitterBackup.Data.Services.ServiceInterfaces;
-using TwitterBackup.Data.Services.Utils;
 
 namespace TwitterBackup.Web.Services
 {
@@ -15,23 +13,36 @@ namespace TwitterBackup.Web.Services
 	{
 		private readonly IWorkSaver workSaver;
 		private readonly UserManager<User> userManager;
-		private readonly IAutoMapper mapper; 
 
-		public AdminUserService(IWorkSaver workSaver, UserManager<User> userManager, IAutoMapper mapper)
+		public AdminUserService(IWorkSaver workSaver, UserManager<User> userManager)
 		{
-			this.workSaver = workSaver ?? throw new ArgumentNullException();
-			this.userManager = userManager;
-			this.mapper = mapper;
+			this.workSaver = workSaver ?? throw new ArgumentNullException(nameof(workSaver));
+			this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
 		}
 
-		public Task<User> GetUserByUsernameAsync(string userName)
-		{
-			throw new NotImplementedException();
-		}
+		public async Task<User> GetUserByUsernameAsync(string userName)
+		=> await this.workSaver.UserRepository.All().FirstOrDefaultAsync(w => w.UserName == userName);
 
 		public void DeleteUserByUserId(string userId)
 		{
-			throw new NotImplementedException();
+			if (userId == null)
+			{
+				throw new ArgumentNullException("UserID cannot be null!");
+			}
+			if (userId == string.Empty)
+			{
+				throw new ArgumentException("UserID cannot be empty!");
+			}
+
+			var user = this.workSaver.UserRepository.All().FirstOrDefault(fd => fd.Id == userId);
+
+			if (user == null)
+			{
+				throw new ArgumentNullException("No such user found!");
+			}
+
+			this.workSaver.UserRepository.Delete(user);
+			this.workSaver.SaveChanges();	///////
 		}
 
 		//public async Task<ICollection<UserDTO>> GetUsersInRolesAsync()
