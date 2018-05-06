fa7bef4f24e9d217b3c36a40f24a9df616aa9164
Aleks Peneva <alexemilpeneva@abv.bg>
2018-05-06 15:18:44 +0300

    Created UserTweeter model to use in admin statistics

--- TwitterBackup/TwitterBackup.Data.Services/Statistics/StatisticsService.cs
+++ TwitterBackup/TwitterBackup.Data.Services/Statistics/StatisticsService.cs
@@ -13,22 +13,41 @@ namespace ReTwitter.Services.Data.Statistics
 	public class StatisticsService : DatabaseService, IStatisticsService
 	{
 		private readonly IRepository<User> userRepository;
-
-		public StatisticsService(IRepository<User> userRepository, IAutoMapper autoMapper, IWorkSaver workSaver) : base(autoMapper, workSaver)
+		private readonly IRepository<UserTweeter> userTweeterRepository;
+		private readonly IRepository<UserTweet> userTweetRepository;
+		
+		public StatisticsService(IRepository<User> userRepository, IRepository<UserTweeter> userTweeterRepository,
+								 IRepository<UserTweet> userTweetRepository,
+								 IAutoMapper autoMapper, IWorkSaver workSaver) : base(autoMapper, workSaver)
 		{
 			this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+			this.userTweeterRepository = userTweeterRepository ?? throw new ArgumentNullException(nameof(userTweeterRepository));
+			this.userTweetRepository = userTweetRepository ?? throw new ArgumentNullException(nameof(userTweetRepository));
+			
 		}
 
 		public ICollection<UserStatisticsDTO> UsersStatistics()
 		{
-			var allUsers = this.userRepository.All().Select(user => new UserStatisticsDTO
+			var allUsers = this.userRepository.All().Select(user => new 
 			{
 				Id = user.Id,
 				UserName = user.UserName,
 				IsDeleted = user.IsDeleted
 			}).ToList();
 
-			
+			var allUsersTweeters = this.userTweeterRepository.All().Select(userTweeter => new
+			{
+				UserName = userTweeter.User.UserName,
+				IsDeleted = userTweeter.IsDeleted
+			}).ToList();
+
+			var allUserTweets = this.userTweetRepository.All().Select(userTweet => new
+			{
+				UserName = userTweet.User.UserName,
+				IsDeleted = userTweet.IsDeleted
+			}).ToList();
+
+
 		}
 	}
 }
